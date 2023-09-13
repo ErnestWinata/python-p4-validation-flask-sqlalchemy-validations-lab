ecb55624c0633ea8ca8481fb51f2ca8b5782aeeb
Ernest <ernestwinata@gmail.com>
2023-09-13 05:42:49 -0700

    initial commit

--- server/models.py
+++ server/models.py
@@ -4,20 +4,69 @@ db = SQLAlchemy()
 
 class Author(db.Model):
     __tablename__ = 'authors'
-    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    def __init__(self, name, phone_number):
+        if not name:
+            raise ValueError("Name cannot be empty.")
+        self.name = name
+        self.phone_number = phone_number
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError("Phone number must be exactly ten digits.")
+        return phone_number
+
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    # Add validations and constraints 
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Title cannot be empty.")
+        return title
+
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Content must be at least 250 characters long.")
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) >= 250:
+            raise ValueError("Summary cannot be more than 250 characters long.")
+        return summary
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ('Fiction', 'Non-Fiction'):
+            raise ValueError("Category must be either 'Fiction' or 'Non-Fiction'.")
+        return category
+    
+    def validate_clickbait_title(self, title):
+        keywords = ["Won't Believe", "Secret", "Top", "Guess"]
+        for keyword in keywords:
+            if keyword in title:
+                return
+        raise ValueError("Title must contain at least one clickbait keyword.")
+
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Title cannot be empty.")
+        self.validate_clickbait_title(title)
+        return title
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
